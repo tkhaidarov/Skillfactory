7d71f74db15fd7b4b74bbb8114ce3347b8c7a7f6
tkhaidarov <168107546+tkhaidarov@users.noreply.github.com>
2024-12-28 12:00:15 +0500

    Исправленные варианты задач 14_5 и 14_7

--- JavaScript_tasks/14_5_Task/script.js
+++ JavaScript_tasks/14_5_Task/script.js
@@ -1,5 +1,20 @@
 const divResult = document.querySelector(".result");
 const button = document.querySelector(".send-btn");
+const img = document.querySelector(".image");
+
+function loadLastImage() {
+  const lastImageData = localStorage.getItem("lastImage");
+  if (lastImageData) {
+    let { width, height, url } = JSON.parse(lastImageData);
+    img.src = url;
+    divResult.innerHTML = `Последнее изображение: ${width}x${height}`;
+  }
+}
+
+function saveImageToLocalStorage(width, height, url) {
+  const imageData = { width, height, url };
+  localStorage.setItem("lastImage", JSON.stringify(imageData));
+}
 
 function initRequest() {
   const widthValue = document.querySelector(".width").value;
@@ -8,8 +23,7 @@ function initRequest() {
   const heigthValue = document.querySelector(".height").value;
   const height = parseInt(heigthValue, 10);
   const heightError = document.querySelector(".height-error");
-  const img = document.querySelector(".image");
-  // img.src = '';
+  img.src = "";
   divResult.innerHTML = "";
   if (isNaN(width) || width < 100 || width > 300) {
     widthError.textContent = "Введите ширину в диапазоне от 100 до 300";
@@ -21,23 +35,27 @@ function initRequest() {
   } else {
     heightError.textContent = "";
   }
-  let url = `https://dummyimage.com/${width}x${height}/`;
+  let url = `https://dummyimage.com/${width}x${height}`;
   fetch(url)
     .then((response) => {
-      if (response.ok) {
+      console.log("response status", response.status);
+      if (!response.ok) {
         throw new Error("Не удалось получить изображение");
       }
       return response.blob();
     })
     .then((blob) => {
-      let objectURL = URL.createObjectURL(blob);
-      img.src = objectURL;
+      console.log("data: ", blob);
+      img.src = URL.createObjectURL(blob);
+      saveImageToLocalStorage(width, height, url);
     })
     .catch((error) => {
-      divResult.innerHTML = error.message;
+      divResult.innerHTML = error.message + " " + error.status;
     });
 }
 
+document.addEventListener("DOMContentLoaded", loadLastImage);
 button.addEventListener("click", () => {
+  console.log("clicked");
   initRequest();
 });
--- JavaScript_tasks/14_7_Task/script.js
+++ JavaScript_tasks/14_7_Task/script.js
@@ -1,44 +1,49 @@
+let btnQuery = document.querySelector(".query-btn");
 
-let btnQuery = document.querySelector('.query-btn');
 function initRequest() {
-    let pageElement = document.querySelector('.page-number');
-    let pageNum = pageElement ? parseInt(pageElement.value, 10) : NaN;
-    let limitElem = document.querySelector('.limit');
-    let limitNum = limitElem ? parseInt(limitElem.value, 10) : NaN;
-    let isPageValid = isNaN(pageNum) || pageNum < 1 || pageNum > 10;
-    let isLimitValid = isNaN(limitNum) || limitNum < 1 || limitNum > 10;
-    let errorText = '';
-    if (isPageValid && isLimitValid) {
-        errorText = 'Номер страницы и лимит вне диапазона от 1 до 10';
-    }else if(isPageValid) {
-        errorText = 'Номер страницы вне диапазона от 1 до 10';
-    }else if(isLimitValid) {
-        errorText = 'Лимит вне диапазона от 1 до 10';
-    }
-    let preResult = document.querySelector('.result');
-    let errorMessage = document.querySelector('.error-message');
-    if(errorText){
-        errorMessage.textContent = errorText;
-       // preResult.textContent = '';
-        return;
-    }else {
-        errorMessage.textContent = '';
-    }
+  let pageElement = document.querySelector(".page-number");
+  let pageNum = pageElement ? parseInt(pageElement.value, 10) : NaN;
+  let limitElem = document.querySelector(".limit");
+  let limitNum = limitElem ? parseInt(limitElem.value, 10) : NaN;
+  let isPageValid = isNaN(pageNum) || pageNum < 1 || pageNum > 10;
+  let isLimitValid = isNaN(limitNum) || limitNum < 1 || limitNum > 10;
+  let errorText = "";
+  if (isPageValid && isLimitValid) {
+    errorText = "Номер страницы и лимит вне диапазона от 1 до 10";
+  } else if (isPageValid) {
+    errorText = "Номер страницы вне диапазона от 1 до 10";
+  } else if (isLimitValid) {
+    errorText = "Лимит вне диапазона от 1 до 10";
+  }
+  let preResult = document.querySelector(".result");
+  let errorMessage = document.querySelector(".error-message");
+  if (errorText) {
+    errorMessage.textContent = errorText;
+    preResult.textContent = "";
+    return;
+  } else {
+    errorMessage.textContent = "";
+  }
 
-    const url = `https://jsonplaceholder.typicode.com/photos?_page=${pageNum}&_limit=${limitNum}`;
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Ошибка запроса: ' + response.status);
-            }
-            return response.json();
-        })
-        .then(data =>{
-            preResult.textContent = JSON.stringify(data, null, 2);
-        })
-        .catch(error => {
-            errorMessage.textContent = 'Ошибка ' + error.message;
-            //preResult.textContent = '';
-        })
+  const url = `https://jsonplaceholder.typicode.com/photos?_page=${pageNum}&_limit=${limitNum}`;
+  fetch(url)
+    .then((response) => {
+      console.log("response status", response.status);
+      if (!response.ok) {
+        throw new Error("Ошибка запроса: " + response.status);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("data: ", data);
+      preResult.textContent = JSON.stringify(data, null, 2);
+    })
+    .catch((error) => {
+      errorMessage.textContent = "Error fetch: " + error.message;
+    });
 }
-btnQuery.addEventListener('click', initRequest);
+
+btnQuery.addEventListener("click", () => {
+  console.log("clicked");
+  initRequest();
+});
