cecde855fd3a7446092f5f13af110d4dadbc59b1
tkhaidarov <168107546+tkhaidarov@users.noreply.github.com>
2025-01-11 11:20:44 +0500

    Модуль 15. Задания

---
+++ Module_15/Task_2/main.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").addEventListener("click", () => {
+    let screenWidth = window.screen.width
+    let screenHeight = window.screen.height
+    alert(`Размеры экрана: ${screenWidth} x ${screenHeight}`)
+})
