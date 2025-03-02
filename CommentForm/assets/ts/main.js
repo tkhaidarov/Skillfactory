33eb8494713a68e631d64d02b2a79688e50e1814
tkhaidarov <168107546+tkhaidarov@users.noreply.github.com>
2025-03-02 10:41:29 +0500

    Comment Element: добавлена фильтрация

--- CommentForm/assets/ts/main.js
+++
@@ -1,7 +0,0 @@
-import { CommentSystem } from './commentSystem.js';
-import { Dropdown } from './dropdown.js';
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded');
-    new CommentSystem();
-    new Dropdown();
-});
